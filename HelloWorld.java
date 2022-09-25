a7bf6b54796e13c4a326c0b73ba71a0eebdbe48d
ryan graham <ryangraham@oakland.edu>
2022-09-25 17:36:28 -0400

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,19 +3,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(x: "Please input an integer:");
-        String (str) = scanner.next();
-        switch (i){
-            case "0":
-                System.out.println(x: "case 0");
-                break;
-            case "1":
-                System.out.println(x: "case 1");
-                break;
-            default:
-                System.out.println(x: "default");
-
-        }
+        System.out.println(x:"metric input to convert:");
+        String str = scanner.nextLine();
+        System.out.println(str);
+        
         scanner.close()
     }
 }
