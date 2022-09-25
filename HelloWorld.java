2422f562f124c43bb4fa4955a32199ec230486e1
ryan graham <ryangraham@oakland.edu>
2022-09-25 17:24:02 -0400

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(x: "Please input an integer:");
+        String (str) = scanner.next();
+        switch (i){
+            case "0":
+                System.out.println(x: "case 0");
+                break;
+            case "1":
+                System.out.println(x: "case 1");
+                break;
+            default:
+                System.out.println(x: "default");
+
+        }
+        scanner.close()
     }
 }
